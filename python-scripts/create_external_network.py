5f8638c64710a628dfd380c90a6197084e95176f
perform <perform@pythagoras.perform.illinois.edu>
2017-02-16 17:05:48 -0600

    added command line arguments

---
+++ python-scripts/create_external_network.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python 
+
+# Note that this file uses the v2_0 authentication 
+# client and thus needs the file overcloudrc 
+# and not the file overcloudrc.v3
+
+
+from neutronclient.v2_0 import client 
+from credentials import get_credentials
+from utils import print_completion_time, exit_with_error_message
+import argparse
+import timeit
+
+# start by obtaining the arguments
+parser = argparse.ArgumentParser(description="Create an external network to which \
+        floating IPs can be associated")
+parser.add_argument("name", help="The name to be used for the created external network")
+parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
+
+args = parser.parse_args()
+
+network_name = args.name
+verbose = args.verbose
+
+start_time = timeit.default_timer()
+
+# get the neutron credentials
+credentials = get_credentials()
+
+# check for missing credentials
+for k, v in credentials.items():
+    if (v == None or v == " "):
+        msg = "Invalid credential value for key %s" % k
+        print_completion_time(start_time)
+        exit_with_error_message(msg)
+
+if verbose:
+    elapsed = timeit.default_timer() - start_time
+    print ("[@ %d] Authenticating to neutron client" % elapsed)
+
+neutron = client.Client(**credentials)
+
+if verbose:
+    elapsed = timeit.default_timer() - start_time
+    print ("[@ %d] Authentication successful" % elapsed)
+
+try:
+    body = {'network': {'name': network_name,
+        'router:external': True,
+        'provider:network_type': 'flat', 
+        'provider:physical_network': 'datacentre'}}
+
+    netw = neutron.create_network (body = body)
+    net_dict = netw['network']
+    network_id = net_dict['id']
+
+    print ('Created Network ID is %s' % network_id)
+finally:
+    elapsed = timeit.default_timer() - start_time
+    print_completion_time (start_time, elapsed)
--- python-scripts/utils.py
+++ python-scripts/utils.py
@@ -6,6 +6,7 @@
 # Link to original source 
 #   https://docs.openstack.org/user-guide/sdk-neutron-apis.html
 
+import timeit
 
 def print_values(val, type):
     if type == 'ports':
@@ -39,3 +40,23 @@ def print_values_server(val, server_id, type):
             for k, v in p.items():
                 print ("%s : %s" %(k, v))
             print ('\n')
+
+
+def exit_with_error_message (msg):
+    """
+    Exit the program early and display an error message
+    """
+    print ("[ERROR] %s\n" % msg)
+    raise SystemExit
+
+def print_completion_time (start_time, elapsed = -1.0):
+    """
+    Print the completion time message. 
+    If elapsed is -1.0, then compute the elapsed time from 
+    the provided start time.
+    """
+
+    if (elapsed == -1.0):
+        elapsed = timeit.default_timer() - start_time
+
+    print ('Execution completed in %d seconds' % elapsed)
